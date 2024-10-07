742c707c0288dc8967015160c10f56e0257737f8
Sylvain Cozenot <sylvaincozenot@outlook.fr>
2024-10-07 14:20:29 +0200

    Update

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-/*#
-  MapSCII - Terminal Map Viewer
-  by Michael Strassburger <codepoet@cpan.org>
-  Discover the planet in your console!
-
-  This scripts boots up the application.
-
-  TODO: params parsing and so on
-#*/
 'use strict';
 const config = require('./src/config');
 const Mapscii = require('./src/Mapscii');
